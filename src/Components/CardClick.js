5c6520115ebc1341a33dc408a2d694fa27a30372
Jxnas999 <jonaskoscielny@gmail.com>
2022-05-15 20:31:06 +0200

    CardClick finish

--- src/Components/CardClick.js
+++ src/Components/CardClick.js
@@ -1,22 +1,40 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../CardClick.css";
 import { AnimeContext } from "../Contexts/Context";
+import { FaStar } from "react-icons/fa";
+
 export default function CardClick() {
   let { id } = useParams();
-  const {anime, setAnime} = useContext(AnimeContext)
-  const [activeAnime, setActiveAnime] = useState('Test')
- 
-  for(let i = 0; i<anime.length; i++){
-  if(anime[i].mal_id === id){
-    console.log(anime[i])
-  }
-}
+  let parseId = parseInt(id); // Convert to Integer, that comparision is possible
 
+  const { anime, setAnime } = useContext(AnimeContext);
+  const [activeAnime, setActiveAnime] = useState([]);
+  console.log(anime);
+  useEffect(() => {
+    anime.filter((item) => {
+      if (item.mal_id === parseId) {
+        return setActiveAnime(item);
+      }
+    });
+  }, []);
 
   return (
     <div className='Card--Click'>
-      
+      <img src={activeAnime.image_url} alt='Animes'></img>
+
+      <section className='info'>
+        <h1>{activeAnime.title}</h1>
+        <section className='ranking'>
+          <FaStar id='icon' />
+          <p>Popularity : {activeAnime.rank}</p>
+        </section>
+        <p>
+          <a href={activeAnime.url} target='_blank' rel='noreferrer'>
+            Learn More
+          </a>
+        </p>
+      </section>
     </div>
   );
 }
